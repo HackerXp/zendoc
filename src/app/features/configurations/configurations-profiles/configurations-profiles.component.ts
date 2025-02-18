04478bd0764e0e91dab27312f22a92037c4032dc
HackerXp <hackerxp33@gmail.com>
2025-02-18 22:45:38 +0100

    add form user

--- src/app/features/configurations/configurations-profiles/configurations-profiles.component.ts
+++ src/app/features/configurations/configurations-profiles/configurations-profiles.component.ts
@@ -1,39 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ModalComponent } from '@shared/components/modal/modal.component';
 import { Modal } from '@shared/interfaces/modal';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-configurations-profiles',
   templateUrl: './configurations-profiles.component.html',
   styleUrls: ['./configurations-profiles.component.css'],
-  imports: [ModalComponent],
+  imports: [ModalComponent, FormsModule, ReactiveFormsModule,],
 })
 export class ConfigurationsProfilesComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private toastr = inject(ToastrService);
+
   modal: Modal = {};
   files: File[] = [];
   chips: string[] = [];
-  formFile!: FormGroup;
+  formUser!: FormGroup;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm = () => {
+    this.formUser = this.fb.group({
+      id: [''],
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      usuario: ['', Validators.required],
+      senha: ['', Validators.required],
+      department: ['', Validators.required],
+      permissao: ['', Validators.required],
+    });
+  };
+
+
 
   closeModal = () => {
     this.modal = {};
   };
-  saveUser = () => {};
+  saveUser = () => { };
 
   openModal() {
     this.modal = {
       isOpen: true,
-      icon: 'icon-add-doc',
-      title: 'Adicionar arquivo ',
+      icon: 'icon-user',
+      title: 'Cadastrar usuários',
       description:
-        'Adicione qualquer tipo de arquivo de forma simples e segura',
+        'Cadastrar  perfis no sistema de forma simples',
       size: 'w-2/5',
       btnCancel: 'Cancelar',
-      btnOK: 'Salvar documento',
+      btnOK: 'Salvar cadastro',
     };
   }
 }
--- src/app/features/configurations/configurations.component.ts
+++ src/app/features/configurations/configurations.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-configurations',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './configurations.component.html',
   styleUrl: './configurations.component.scss'
 })

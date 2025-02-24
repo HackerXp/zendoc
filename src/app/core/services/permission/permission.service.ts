da961dd302697d9614c3643d3fbefdbe91a290d4
HackerXp <hackerxp33@gmail.com>
2025-02-24 18:14:40 +0100

    feat: configurations-1

--- src/app/core/services/permission/permission.service.ts
+++ src/app/core/services/permission/permission.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
+import { Permission } from '@shared/interfaces/permission';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PermissionService {
+export class PermissionService extends BaseService {
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getPermission = (): Observable<Permission> => {
+    return this.http.get<Permission>(`${this.apiURL}/?rota=listar-todos-departamento`);
+  };
+
+  savePermission = (formData: FormData): Observable<Permission> => {
+    return this.http.post<Permission>(`${this.apiURL}/?rota=cadastrar-departamento`, formData)
+  }
+
+  editPermission = (formData: FormData): Observable<Permission> => {
+    return this.http.put<Permission>(`${this.apiURL}/?rota=editar-departamento`, formData)
+  }
+
 }
